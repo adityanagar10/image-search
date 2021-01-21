de374115b633dc7b896a079b5dfea89518c20681
adityanagar07 <74241019+adityanagar07@users.noreply.github.com>
2021-01-21 22:51:15 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header(){
+    return(
+        <header className="app-header text-2xl px-6 py-4">Made by: Aditya Nagar</header>
+    )
+}
+
+export default Header
